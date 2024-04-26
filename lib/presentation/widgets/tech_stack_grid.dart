885d719715177a1199d9d6479acbd90899173c06
Mujahedul Islam <dev.muj.i@icloud.com>
2024-04-26 20:49:26 +0600

    Update tech_stack_grid.dart

--- lib/presentation/widgets/tech_stack_grid.dart
+++ lib/presentation/widgets/tech_stack_grid.dart
@@ -2,6 +2,7 @@ import 'package:dev_folio/presentation/models/tech_stack_model.dart';
 import 'package:dev_folio/presentation/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
 class TechStackGridView extends StatelessWidget {
   const TechStackGridView(
@@ -20,13 +21,13 @@ class TechStackGridView extends StatelessWidget {
         ),
         const SizedBox(height: 30),
         SizedBox(
-          height: techSdkItems.length > 8 ? 200 : 100,
+          height: Get.width < 700 ? 100: 300,
           child: GridView.builder(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 8,
-              // crossAxisSpacing: Get.width < 900 ? 5 : 0,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: Get.width < 700 ? 4 : 8,
+              crossAxisSpacing: Get.width < 700 ? 5 : 1,
               // mainAxisSpacing: Get.width < 900 ? 5 : 0,
             ),
             itemCount: techSdkItems.length,
